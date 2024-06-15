1a34eb9f3f61f2c55658a1355ed661483bb91e3b
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-15 21:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,10 +22,11 @@ class Square extends Rectangle {
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
 describe('My Test Suite', function() {
   it('calculates area and perimeter correctly - 1', function() {
     const square = new Square(7);
     expect(square.getArea()).to.equal(49); // checks if the area is correctly calculated
     expect(square.getPerimeter()).to.equal(28); // checks if the perimeter is correctly calculated
   })
-})
+});
