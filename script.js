10a489e0fdef92218d087cdf8764fbb14a1021ce
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-15 20:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //complete this code
-class Rectangle {}
+class Rectangle(width,height) {
+	this.width=width;
+	this.height=height;
+	getArea(){
+		return width*height;
+	}
+}
 
-class Square extends Animal {}
-
+class Square extends Rectangle {
+	getPerimeter(){
+		return 4*width;  
+	}
+}
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
